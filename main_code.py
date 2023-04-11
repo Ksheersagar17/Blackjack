ade8c6944abe885af96724b33a21a76ba4d0f50b
Ksheersagar17 <129789456+Ksheersagar17@users.noreply.github.com>
2023-04-11 21:52:21 +0530

    path update

--- main_code.py
+++ main_code.py
@@ -1,12 +1,10 @@
 import random
 
-values={'Two':2,'Three':3,'Four':4,'Five':5,'Six':6,'Seven':7,'Eight':8,
-        'Nine':9,'Ten':10,'Jack':10,'Queen':10,'King':10,'Ace':11}
+values={'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'10':10,'J':10,'Q':10,'K':10,'A':11}
 
 suit=('Hearts','Diamonds','Clubs','Spade')
 
-rank=('Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','Jack',
-      'Queen','King','Ace')
+rank=('2','3','4','5','6','7','8','9','10','J','Q','K','A')
 
 playing = True
 
@@ -18,7 +16,7 @@ class Card:
         self.rank=rank
     
     def __str__(self):
-        return self.rank + " of " + self.suit
+        return "assets/" + self.suit + "/" + self.rank + ".png"
     
 class Deck:
     def __init__(self):
